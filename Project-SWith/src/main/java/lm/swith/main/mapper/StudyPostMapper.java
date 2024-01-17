d58a3318bf86f6e0d7b804b2107592c579a38d9c
Kaye <139112573+2Mihye@users.noreply.github.com>
2024-01-17 19:00:47 +0900

    mapper update

--- Project-SWith/src/main/java/lm/swith/main/mapper/StudyPostMapper.java
+++ Project-SWith/src/main/java/lm/swith/main/mapper/StudyPostMapper.java
@@ -9,13 +9,24 @@ import lm.swith.main.vo.StudyPost;
 
 @Mapper
 public interface StudyPostMapper {
+	// 스터디 목록
 	List<StudyPost> getAllStudyPost();
 
+	// 스터디 상세 페이지
 	StudyPost getStudyPostById(@Param("post_no") Long post_no);
 	
+	// 스터디 분류
+	StudyPost getStudiesBySelect();
+	
+	// 스터디 제목+내용 검색
+	StudyPost getStudiesBySearch();
+	
+	// 스터디 등록
 	void insertStudyPost (StudyPost studyPost);
 	
+	// 스터디 수정
 	void updateStudyPost (StudyPost studyPost);
 	
+	// 스터디 삭제
 	void deleteStudyPost (Long post_no);
 }
