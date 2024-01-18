fd03a10208db2fccd1fff9f2d2331b4ed71974d3
Kaye <139112573+2Mihye@users.noreply.github.com>
2024-01-18 14:04:29 +0900

    mapper update

--- Project-SWith/src/main/java/lm/swith/main/mapper/StudyPostMapper.java
+++ Project-SWith/src/main/java/lm/swith/main/mapper/StudyPostMapper.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Param;
 import lm.swith.main.model.StudyPost;
 
 @Mapper
-public interface StudyPostMapper 
+public interface StudyPostMapper {
 	
 	// 스터디 목록
 	List<StudyPost> getAllStudyPost();
@@ -28,7 +28,6 @@ public interface StudyPostMapper
 	// 스터디 조건 검색
 	List<StudyPost> getStudiesBySelect(String recruit_type, String study_method, String study_location, Long skill_no);
 		
-	
 	// 스터디 제목+내용 검색
 	List<StudyPost> getStudiesBySearch(String study_title, String study_content);
 	
