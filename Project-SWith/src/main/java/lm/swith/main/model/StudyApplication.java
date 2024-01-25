f544b5bb238a8c67ac5a3d62504c3af86c24486d
Kaye <139112573+2Mihye@users.noreply.github.com>
2024-01-25 15:27:27 +0900

    correcting queries

--- Project-SWith/src/main/java/lm/swith/main/model/StudyApplication.java
+++ Project-SWith/src/main/java/lm/swith/main/model/StudyApplication.java
@@ -4,5 +4,9 @@ import lombok.*;
 
 @Getter @Setter
 public class StudyApplication {
-	
+	private Long post_no;
+	private Long user_no;
+	private String status;
+	private int max_study_applicants;
+	private int study_join_member;
 }
