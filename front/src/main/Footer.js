cf24de35e32c5810b27b2924e4fb127bd52a6f10
Kaye <139112573+2Mihye@users.noreply.github.com>
2024-02-15 18:11:09 +0900

    almost

--- front/src/main/Footer.js
+++ front/src/main/Footer.js
@@ -9,13 +9,13 @@ function Footer() {
           이용약관 |
         </a>
         <a href="https://pebble-lady-eb0.notion.site/S-With-ba28725450474a66bbd9efe5e48700c1?pvs=4">
-          개인정보처리방침 |
+          &nbsp;개인정보처리방침 |
         </a>
         <a href="https://pebble-lady-eb0.notion.site/S-With-5f42e8a793904d2e837ad8963eb31b55?pvs=4">
-          제휴제안 |
+          &nbsp;제휴제안 |
         </a>
         <a href="https://pebble-lady-eb0.notion.site/S-With-2773401d4a8140c495261468e254dc8f?pvs=4">
-          문의사항
+          &nbsp;문의사항
         </a>
         <p style={{ marginTop: "20px" }}>
           Copyright &copy;S.With. All rights reserved
